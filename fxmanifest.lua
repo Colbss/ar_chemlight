44c86e152b05cf4c9a1ed02899a7bd67ac73c782
ColbyD <colbydeed@gmail.com>
2025-07-04 21:28:08 +1200

    init

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'Co1bss'
+description 'Aether Roleplay - Item Throw Prop'
+version '1.0.0'
+
+files {
+	"data/weaponarchetypes.meta",
+	"data/weaponanimations.meta",
+	"data/pedpersonality.meta",
+	"data/weapons.meta",
+}
+
+data_file "WEAPON_METADATA_FILE" "data/weaponarchetypes.meta"
+data_file "WEAPON_ANIMATIONS_FILE" "data/weaponanimations.meta"
+data_file "WEAPONINFO_FILE" "data/weapons.meta"
+data_file "PED_PERSONALITY_FILE" "data/pedpersonality.meta"
